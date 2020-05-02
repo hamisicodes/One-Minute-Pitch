1609df9f047e2a9421f51d979922f6adc4e93601
hamisicodes <newhamisi@gmail.com>
2020-05-02 15:39:02 +0300

    add code to the register view function and its template

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,27 @@
 from . import auth
-fromfl
+from flask import render_template,redirect,url_for
+from .. import db
+from .forms import RegistrationForm
+from ..models import User
 
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    return render_template('login.html')
 
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
-    return render_template('auth/register.html')
+    form = RegistrationForm()
+    
+    if form.validate_on_submit():
+        user = User(username = form.username.data ,email= form.email.data , password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+
+    return render_template('register.html',registration_form = form)
 
 
 @auth.route('/logout')
