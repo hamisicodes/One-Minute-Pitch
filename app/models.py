212f9e86dd30b3dcccff74c4a83657aa1366d411
hamisicodes <newhamisi@gmail.com>
2020-05-02 14:59:15 +0300

    Implement the loader for the login manager

--- app/models.py
+++ app/models.py
@@ -1,12 +1,19 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer ,primary_key = True)
     username = db.Column(db.String(255), index = True)
-    email = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255),unique = True, index = True)
     pass_secure = db.Column(db.String(255))
     bio = db.Column(db.String(255))
     pitches  = db.relationship('Pitch', backref = 'user' , lazy = 'dynamic')
