9903afff828b85d9e5c018591b6c78a9cc8131ae
hamisicodes <newhamisi@gmail.com>
2020-05-03 12:57:22 +0300

    Include a form to post a pitch

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,12 @@
 from . import main
-from flask import render_template
+from flask import render_template,request
 from flask_login import login_required
 
 @main.route('/')
 def index():
     title = 'Hello World'
     return render_template('index.html', title = title)
-    
+
 
 @main.route("/pitch" ,methods=["GET", "POST"])
 @login_required
